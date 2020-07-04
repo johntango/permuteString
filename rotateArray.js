a01165d3d206e051a3edef993abbcad0eee9ed3d
John Williams <jrwtango@gmail.com>
2020-07-04 10:03:24 -0400

    save

--- rotateArray.js
+++ rotateArray.js
@@ -9,6 +9,7 @@ const T = [
   [0, 0, 1],
 ];
 function transpose(A) {
+  // we need only swap upper triangle with lower
   let N = A.length;
   for (let i = 0; i < N - 1; i++) {
     for (let j = i + 1; j < N; j++) {
@@ -23,7 +24,7 @@ function swap(A, i, j, k, l) {
 }
 function exchangeCols(A) {
   // work in from outside
-  let N = A.length; //
+  let N = A.length; // we need only swap N/2 (rounded down)
   let n = Math.floor(N / 2); // round down to integer
   for (let col = 0; col < n; col++) {
     for (let row = 0; row < N; row++) {
